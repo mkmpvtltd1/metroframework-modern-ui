23ee9e2b92f24662461069669c1f915544307ffe
Manoj Mahato <mmahato@braindigit.com>
2016-08-01 12:54:34 +0545

    Watermark TextBox added

---
+++ MetroFramework/Controls/MetroTextBoxWatermark.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MetroFramework.Controls
+{
+    [Designer("MetroFramework.Design.Controls.MetroTextBoxDesigner, " + AssemblyRef.MetroFrameworkDesignSN)]
+    public class MetroTextBoxWatermark : MetroTextBox
+    {
+        private const uint ECM_FIRST = 0x1500;
+        private const uint EM_SETCUEBANNER = ECM_FIRST + 1;
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, [MarshalAs(UnmanagedType.LPWStr)] string lParam);
+
+        private string watermarkText;
+        public string WatermarkText
+        {
+            get { return watermarkText; }
+            set
+            {
+                watermarkText = value;
+                SetWatermark(watermarkText);
+            }
+        }
+
+        private void SetWatermark(string watermarkText)
+        {
+            SendMessage(this.Handle, EM_SETCUEBANNER, 0, watermarkText);
+        }
+
+    }
+}
